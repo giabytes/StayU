125262964387a3e54234dd7c2715c24e68ef99b4
Nath-Ramirez <rhnathaly2@gmail.com>
2025-11-12 01:11:17 -0500

    Update userService.js

--- frontend/src/services/userService.js
+++ frontend/src/services/userService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE = import.meta.env.VITE_API_USERS || "/api/user/auth/login";
+const BASE = import.meta.env.VITE_API_USERS || "/api/user";
 
 export const getStudents = async () => {
   // ajusta la ruta si en tu API de Users es distinta (ej: /users or /students)
